e1429ea89e7cf4dd2367c0b9c3932625d4e633e9
Leandro <statuss@hotmail.com>
2015-12-24 16:47:36 -0300

    subindo arquivos

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,104 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Laravel</title>
+
+        <link href="https://fonts.googleapis.com/css?family=Lato:100" rel="stylesheet" type="text/css">
+
+        <style>
+            html, body {
+                height: 100%;
+            }
+
+            body {
+                margin: 0;
+                padding: 0;
+                width: 100%;
+                display: table;
+                font-weight: 100;
+                font-family: 'Lato';
+            }
+
+            .container {
+                text-align: center;
+                display: table-cell;
+                vertical-align: middle;
+            }
+
+            .content {
+                text-align: center;
+                display: inline-block;
+            }
+
+            .title {
+                font-size: 96px;
+            }
+        </style>
+    </head>
+    <body>
+        <div class="container">
+            <div class="content">
+                <div class="title">Laravel 5</div>
+            </div>
+        </div>
+
+
+<ul class="nav nav-tabs">
+  <li class="active"><a href="#">Home</a></li>
+  <li><a href="#">Menu 1</a></li>
+  <li><a href="#">Menu 2</a></li>
+  <li><a href="#">Menu 3</a></li>
+</ul>
+
+
+  <div class='container'>
+        
+        <div class="span12">
+        
+    
+                <div id="rootwizard" class="nav nav-tabs">
+                    <ul>
+                        <li><a href="#tab1" data-toggle="tab">First</a></li>
+                        <li><a href="#tab2" data-toggle="tab">Second</a></li>
+                        <li><a href="#tab3" data-toggle="tab">Third</a></li>
+                        <li><a href="#tab4" data-toggle="tab">Forth</a></li>
+                        <li><a href="#tab5" data-toggle="tab">Fifth</a></li>
+                        <li><a href="#tab6" data-toggle="tab">Sixth</a></li>
+                        <li><a href="#tab7" data-toggle="tab">Seventh</a></li>
+                    </ul>
+                    <div class="tab-content">
+                        <div class="tab-pane" id="tab1">
+                          1
+                        </div>
+                        <div class="tab-pane" id="tab2">
+                          2
+                        </div>
+                        <div class="tab-pane" id="tab3">
+                            3
+                        </div>
+                        <div class="tab-pane" id="tab4">
+                            4
+                        </div>
+                        <div class="tab-pane" id="tab5">
+                            5
+                        </div>
+                        <div class="tab-pane" id="tab6">
+                            6
+                        </div>
+                        <div class="tab-pane" id="tab7">
+                            7
+                        </div>
+                        <ul class="pager wizard">
+                            <li class="previous first" style="display:none;"><a href="#">First</a></li>
+                            <li class="previous"><a href="#">Previous</a></li>
+                            <li class="next last" style="display:none;"><a href="#">Last</a></li>
+                            <li class="next"><a href="#">Next</a></li>
+                        </ul>
+                    </div>  
+                </div>
+        </div>
+    </div>
+
+
+    </body>
+</html>
